fd44c0f95970e5939e47f021c2c67d5da20e1f4d
Yessica Ríos <yskrios@gmail.com>
2021-02-10 19:44:48 -0500

    slot

---
+++ slot/my-element.js
@@ -0,0 +1,37 @@
+class myElementm extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+        <section>
+          <h2>
+            <slot></slot>
+          </h2>
+
+        </section>
+        ${this.getStyle()}
+      `;
+
+    return template;
+  }
+
+  getStyle() {
+    return `
+    <style>
+      h2{
+        color:red;
+      }
+    </style>`;
+  }
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+  connectedCallback() {
+    this.render();
+  }
+}
+
+customElements.define("my-element", myElementm);
