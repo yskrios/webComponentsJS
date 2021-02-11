2357abe68d6946976f56d7032e64b0ef9e2947bc
Yessica Ríos <yskrios@gmail.com>
2021-02-10 22:29:49 -0500

    se agrega elementos como atributos

--- slot/my-element.js
+++ slot/my-element.js
@@ -2,18 +2,18 @@ class myElementm extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    this.title = this.getAttribute("title");
+    this.parrafo = this.getAttribute("parrafo");
+    this.img = this.getAttribute("img");
   }
   getTemplate() {
     const template = document.createElement("template");
     template.innerHTML = `
         <section>
-          <h2>
-            <slot name="title"></slot>
-          </h2>
+          <h2>${this.title}</h2>
           <div>
-            <p>
-              <slot name="parrafo"></slot>
-            </p>
+            <p>${this.parrafo}</p>
+            <img src=${this.img}>
           </div>
         </section>
         ${this.getStyle()}
