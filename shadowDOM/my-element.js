4525903b0fc65154a5592c7f9a1c942de9b10193
Yessica Ríos <yskrios@gmail.com>
2021-02-10 19:34:37 -0500

    agregando shadowDOM

---
+++ shadowDOM/my-element.js
@@ -0,0 +1,37 @@
+class myElementm extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+        <section>
+          <h2>Hola mundo cruel</h2>
+          <div>
+            <p>Soy mas texto ejemplo</p>
+          </div>
+        </section>
+        ${this.getStyle()}
+      `;
+
+    return template;
+  }
+
+  getStyle() {
+    return `
+    <style>
+      h2{
+        color:red;
+      }
+    </style>`;
+  }
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+  connectedCallback() {
+    this.render();
+  }
+}
+
+customElements.define("my-element", myElementm);
